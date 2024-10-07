96ac7b2f1e1a8570b13eeb4a1da7a72d3cdb4d3c
JacobZimling <68955961+JacobZimling@users.noreply.github.com>
2024-10-07 16:53:12 +0200

    Added barcode scanner

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,19 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/scanner')
-def scanner():
-    return render_template('scanner.html')
+@app.route('/qr_scanner')
+def qr_scanner():
+    return render_template('qr_scanner.html')
+
+@app.route('/barcode_scanner')
+def barcode_scanner():
+    return render_template('barcode_scanner.html')
+
+@app.route('/display_data', methods=['POST'])
+def display_data():
+    qr_data = request.form.get('qr_data')
+    # Process the QR/Barcode data here
+    return f'Detected data: {qr_data}'
 
 if __name__ == '__main__':
     app.run(debug=True)
